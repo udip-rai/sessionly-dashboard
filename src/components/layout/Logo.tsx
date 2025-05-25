5706a50381e063fb8dc69d24a833d7c64e358b23
udip-rai <atomicdeveloper0329@gmail.com>
2025-05-25 22:00:46 +0545

    refractor: enchanced the designs and working solution for image

--- src/components/layout/Logo.tsx
+++ src/components/layout/Logo.tsx
@@ -1,4 +1,5 @@
 import { clsx } from "clsx";
+import { assets } from "../../constants/assets";
 
 export const Logo = ({
   className = "",
@@ -6,9 +7,8 @@ export const Logo = ({
 }: { className?: string } & React.HTMLAttributes<HTMLDivElement>) => {
   return (
     <div className={clsx("relative w-56 h-20 max-h-20 p-0 m-0", className)}>
-      {" "}
       <img
-        src="/src/assets/images/sessionly-logo.png"
+        src={assets.logo}
         alt="Sessionly Logo"
         className="object-cover w-full h-full"
         {...props}
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,21 @@
 /// <reference types="vite/client" />
 
 declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
   const value: string;
   export default value;
 }
